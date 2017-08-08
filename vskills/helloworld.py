9fcfa1640f8374528e554b3c952bf7e4cf3416f0
Govindarajan Vishnuchithan <govindarajan.vishnuchithan@altisource.com>
2017-08-08 19:07:30 +0530

    Changes done on 08/08/17

---
+++ vskills/helloworld.py
@@ -0,0 +1,2 @@
+#!/c/Users/vishnuch/AppData/Local/Programs/Python/Python36-32/python
+print ("Hello World")
---
+++ vskills/strings.py
@@ -0,0 +1,25 @@
+print ('single quote string')
+print ("double quote string")
+print (""" triple quote multi line string.
+        Second line.
+        Third line. """)
+print (''' triple quote multi line string.
+        Second line.
+        Third line. ''')
+print ('what\'s your name?')
+print ("what's your name?")
+print (" First line \
+        Second line ")
+print ("Newlines are indicated by \n")
+#To specify raw string prefix with 'r' or 'R'
+print (r"Newlines are indicated by \n")
+print ("String" "Concatenation")
+
+# formatting
+
+age=35
+name='Govind'
+print ('{0} is {1} years old'.format(name,age))
+print ('{0} wants to learn Python'.format(name))
+
+
