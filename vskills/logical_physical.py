f2a019371f34f166b81069beebc0181d22c4735e
Govindarajan Vishnuchithan <govindarajan.vishnuchithan@altisource.com>
2017-08-09 17:57:21 +0530

    practice done on 9/8/17

---
+++ vskills/logical_physical.py
@@ -0,0 +1,10 @@
+# ; specifies logical line and next line seen in editor is physical line
+i=2;
+print (i);
+# This is same
+i=2
+print (i)
+# This is also same
+i=3;print (i)
+print\
+(i)
---
+++ vskills/operators.py
@@ -0,0 +1,11 @@
+print (1+3)
+print (1-3)
+print (1*3)
+print (1/3)
+print (2**3)
+print (5<<1)
+print (5>>1)
+a = 3
+print (a)
+a *= 3
+print (a)
