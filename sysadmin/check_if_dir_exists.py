6ddaedc5815643d0f4bc245b2debd8928196a4bd
Govindarajan V <govindarajanv@gmail.com>
2018-02-14 03:09:56 +0000

    Python scripting for checking directory using subprocess

--- sysadmin/check_if_dir_exists.py
+++ sysadmin/check_if_dir_exists.py
@@ -1,7 +1,7 @@
 #python script to check if a directory exists or not
 import os
 
-if os.path.isdir("D:\devops\Materials\python"):
+if os.path.isdir("/home/vagrant/workspace/python/sysadmin"):
     print ("Yes, Directory exists")
 else:
     print ("No, Directory does not exist")
