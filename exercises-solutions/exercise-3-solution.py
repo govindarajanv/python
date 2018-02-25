20dc628555ac0e5685db4705cd560a5d84d71cba
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-02-25 17:28:31 +0530

    Exercise 3 solutions

---
+++ exercises-solutions/exercise-3-solution.py
@@ -0,0 +1,34 @@
+# Concatenate two strings using both '+' and ','
+
+print ("hi," + "how are you?")
+print ("Govind" , "Rajan") #automatic space is added
+
+#Concatenate a string and an integer using '+' and ','
+print ("hi," , 5)
+print ("hi," + str(5))
+
+#addtion of a string and an integer by converting the string number to integer
+print ('8',5)
+print (int('8'),5)
+print (int('8')+5)
+
+#Find the length of a string
+print ("length of the string 'python' is",len("python"))
+
+#Print a string with double quotes in one of the sub strings
+print ("I told him \"Bye\"")
+
+# Print a string in a new line character
+print ("please enter \n for new line character")
+
+#Print a string with '\n' and escape the same in the next line (don't use 'r')
+print ("please enter \\n for new line character")
+
+#Print a string with '\n' and escape the same in the next line (use 'r')
+print (r"Please enter \n for new line character")
+
+#Print a string 5 times
+print ("Calling out 5 times! " * 5)
+
+#Print the 3 character from the given string
+
