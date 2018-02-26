3c3dc0f771e53c6e2681fe721ab93a34e7192fc8
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-02-26 10:47:38 +0530

    completed some exercises in strings

--- exercises-solutions/exercise-3-solution.py
+++ exercises-solutions/exercise-3-solution.py
@@ -36,3 +36,29 @@ print (name[0:3])
 
 #Print the last but one character from the given string
 print (name[-2])
+
+#Print all characters starting from 3rd postion till the end
+print (name[2:])
+
+#Print all characters till 6th position
+print (name[:6])
+
+#Print all characters from 2nd to 4th positions
+print (name[1:4])
+
+#print with an index of length  and -(length+1) and see how python reacts to it
+print ("length of name:", len(name)) 
+#print ("name[length]:",name[len(name)]) #index out of range error
+#print ("name[-(length+1)]:",name[-(len(name)+1)]) #index out of range error
+print ("name[length]:",name[len(name)-1]) 
+print ("name[-(length)]:",name[-(len(name))]) 
+
+#concate string in 3 different ways - using %s, join and +
+action = "codes python"
+finalString = name + " " + action
+print ("Using '+': concatenated string is:",finalString)
+print ("Using percentage, concatenated string is:%s %s" % (name,action))
+str_list = []
+str_list.append(name)
+str_list.append(action)
+print("using join, concatenated string is: %s" % ''.join(str_list))
