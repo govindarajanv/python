ac56d4f10e83523c044051cef4d8b09b18816d2e
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-03-08 20:01:42 +0530

    Added code for while and for loops

---
+++ exercises-solutions/exercise-5-solution.py
@@ -0,0 +1,19 @@
+# Create an infinite loop using while
+#while True:
+#    print ("Infinite loop")
+#Print a number 10 times using while loop
+print ("Print 1 to 10 using while loop")
+num = 0
+while num < 10:
+    num = num + 1
+    print (num)
+
+#for loop
+print ("Printing the contents of a list")
+numList = [1,2,5,3,6,8,37,47,32 ]
+for num in numList:
+    print (num)
+
+print ("Printing a range of numbers")
+for num in range(0,100):
+    print (num)
