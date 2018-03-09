e0e92f2b21f4e626f10b083fa74f84f3e4f9d2b4
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-03-09 19:19:42 +0530

    Added code for exercise#4

---
+++ exercises-solutions/exercise-4-solution.py
@@ -0,0 +1,14 @@
+# Assign two variables simultaneously (packing and upacking)
+
+a,b = 4,5
+print ("a=",a)
+print ("b=",b)
+
+# Assign a function to a variable
+
+def test():
+    return 6
+
+c = test();
+
+print ("c=",c)
