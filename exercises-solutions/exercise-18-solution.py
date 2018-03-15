1d375b5d9a7119bd6ec81593b4e7a38a44c36fed
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-03-15 18:48:31 +0530

    Python csv parsing operations

---
+++ exercises-solutions/exercise-18-solution.py
@@ -0,0 +1,13 @@
+#print two dimensional list
+list1 = [[1,2],[2,3],[3,4],[4,5],[5,6]]
+print (list1)
+
+print (list1[1][0])
+#python does not enforce standards
+list2 = [
+        [[1,2],[2,3]],
+        [[3,4],[4,5]],
+         [5,6]
+        ]
+print (list2[1][0])
+print (list2[1][0][1])
