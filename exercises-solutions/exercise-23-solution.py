d3e85de2526276b729b8d48f3efa0bf43a2defbf
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-03-16 19:47:24 +0530

    changes done to test additional functions

---
+++ exercises-solutions/exercise-23-solution.py
@@ -0,0 +1,34 @@
+#
+
+num1 = -5
+num2 = 8
+
+print ("num1=",num1)
+print ("abs(num1)=",abs(num1))
+
+x= [7,3,6,4,2,9]
+print (max(x))
+print (min(x))
+
+y = 1.236
+print (round(y))
+
+import math
+print ("floor of", y, math.floor(y))
+print ("ceil of",y, math.ceil(y))
+
+valString = '23'
+print(int(valString))
+print (float(valString))
+
+strVal = 77
+print (str(strVal))
+
+
+#help
+help()
+#urllib
+#import time
+#help(time)
+
+
