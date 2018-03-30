be9e9e10a6428e9b10c6b42958d3c20d13d94804
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-03-30 08:24:18 +0530

    python based fundamental screener

--- investing/fi_fetch_price2book.py
+++ investing/fi_fetch_price2book.py
@@ -5,10 +5,9 @@ from urllib.request import urlopen
 goldGrade = ['HDFCBANK.NS','HDFC.NS']
 silverGrade = ['GAIL.NS','NMDC.NS'] 
 
-def getStatsFromYahoo(stock):
+def getStatsFromVR(stock):
     try:
         base_url = "https://www.valueresearchonline.com/stocks/snapshot.asp?code=" + stock
-        print (base_url)
         headers = {}
         headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
         request = urllib.request.Request(base_url,headers=headers)
@@ -17,9 +16,12 @@ def getStatsFromYahoo(stock):
         #print (sourceCode)
 
         price_to_book = sourceCode.split("                            P/B: ")[1].split("\r\n")[0]
-        print ("Price to Book Ratio:", price_to_book)
+        stock_name = sourceCode.split("<title> ")[1].split(". - Stock Snapshot - Value Research Online</title>")[0]
+        #max_pe = sourceCode.split("\$\('#pb_slider'\).slider")[1].split("min: parseFloat\('")[1].split("\)\,")[0]
+        print ("Price to Book Ratio of %s is %s" % (stock_name,price_to_book))
+        #print (max_pe)
         
     except Exception as e:
-        print ("Failed in the getStatsFromYahoo method",e)
+        print ("Failure in the getStatsFromVR method",e)
 
-getStatsFromYahoo('1459')
+getStatsFromVR('1459')
