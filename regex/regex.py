a7f1872423c2461bf08afdc63aee1f293e8d2ee5
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-04-26 19:06:51 +0530

    regex practice

--- regex/regex.py
+++ regex/regex.py
@@ -1,10 +1,12 @@
 import re
 
+# Methods like match, search, finall and sub
+
 pattern = r"spam"
 
 print ("\nFinding \'spam\' in \'eggspamsausagespam\'\n")
 
-print ("Usage of match - exact match")
+print ("Usage of match - exact match as it looks at the beginning of the string")
 if re.match(pattern, "eggspamsausagespam"):
    print("Match")
 else:
@@ -18,3 +20,20 @@ else:
     
 print ("\nUsage of findall - returns list of substrings matching the pattern")
 print(re.findall(pattern, "eggspamsausagespam"))
+
+print ("\nFinding \'pam\' in \'eggspamsausages\'\n")
+pattern1 = r"pam"
+match = re.search(pattern1, "eggspamsausages")
+if match:
+    # returns string matched
+    print(match.group())
+    print(match.start())
+    print(match.end())
+    # positions as tuples
+    print(match.span())
+
+str = "My name is David. Hi David."
+print ("old string:",str)
+pattern = r"David"
+newstr = re.sub(pattern, "John", str)
+print ("new string:",newstr)
