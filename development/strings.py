13d5521263d306c505a4331e48ea741a8fe924f9
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-04-27 08:22:36 +0530

    linuxacademy python training exercises

---
+++ development/strings.py
@@ -0,0 +1,16 @@
+print ("Hello World")
+x="""
+Hi
+How are you?
+"""
+print (x)
+print ("pass" + "word")
+#if does not exist,then return is -1
+print ("trouble".find('s'))
+
+#character position if found
+print ("trouble".find('r'))
+print ("trouble".find('ro'))
+
+print ("TrouBle".lower())
+print ("pass" + "\nword")
