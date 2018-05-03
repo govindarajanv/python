fbbc2c42889485d6e227a036a8bcd330ea8b3fb4
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-05-03 08:05:37 +0530

    exercises from linux academy python

---
+++ linux_academy/dictionaries.py
@@ -0,0 +1,6 @@
+ages = {"govind" : 35, "raghav" : 45, "seshu" : 41}
+print (ages)
+del ages['seshu']
+print (ages)
+ages.pop('raghav')
+print (ages)
---
+++ linux_academy/tuples.py
@@ -0,0 +1,6 @@
+print ("Name of students are %s,%s and %s" % ("Ram","Raj","Balu"))
+
+# Error
+#print ("Name of students are %s,%s and %s" % ("Ram","Raj","Balu","Rohit"))
+
+print ("Name of the student is %s" % "Ram")
