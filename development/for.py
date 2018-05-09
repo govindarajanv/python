54a402e9173355d157cfeec7e01cd10c751c2166
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-05-09 11:41:26 +0530

    some additional exercises were practised

---
+++ development/for.py
@@ -0,0 +1,12 @@
+list_of_points = [(1, 2), (2, 3), (3, 4)]
+
+for x, y in list_of_points:
+    print(f"x: {x}, y: {y}")
+
+ages = {'kevin': 59, 'bob': 40, 'kayla': 21}
+
+for name, age in ages.items():
+    print(f"Person Named: {name}")
+    print(f"Age of: {age}")
+
+
