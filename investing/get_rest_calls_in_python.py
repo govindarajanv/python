f0473adef1cd32452b5993671072ba20ec061ddf
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-06-10 14:58:50 +0530

    fixed issues related to stock analyzer

--- investing/get_rest_calls_in_python.py
+++ investing/get_rest_calls_in_python.py
@@ -10,69 +10,86 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 #####################################################
 #   Query the source to download the data
 #####################################################
-access_token="zOGGwxBurJukw8aJ45LfUmjFFhjA"
-#URL = "https://api-global.morningstar.com/sal-service/v1/stock/valuation/v2/0P0000AXIE?access_token="+ access_token
-URL = "https://api-global.morningstar.com/sal-service/v1/stock/valuation/v2/0p0000hj9s?access_token="+ access_token
- 
-head = {"Accept":"applicaiton/json", "Content-type": "application/json", }
-	
- 
-# defining a params dict for the parameters to be sent to the API
-cookies = dict(mid='17423110563646529469',Hint='usw2e10')
- 
-# sending get request and saving the response as response object
-r = requests.get(url = URL, headers=head,cookies=cookies,verify=False)
- 
-# extracting data in json format
-data = r.json()
-#print (data)
- 
- 
-#####################################################
-#   Get Historical Averages
-#####################################################
+access_token="vDp5ATqCIMjhZEh7G2i35qtNPK8p"
 
-#print (data['Collapsed'])
-list_from_json = data['Collapsed']['rows'][0]['datum']
-#print (list_from_json)
-price_to_sales = [float(i) for i in list_from_json]
-#print (sum(price_to_sales[:-3]))
-print ("Average PS:",statistics.mean(price_to_sales[:-3]))
-avg_price_to_sales = statistics.mean(price_to_sales[:-3])
-#
-#
-list_from_json = data['Collapsed']['rows'][1]['datum']
-#print (list_from_json)
-price_to_earning = [float(i) for i in list_from_json]
-#print (sum(price_to_earning[:-3]))
-print ("Average PE:",statistics.mean(price_to_earning[:-3]))
-avg_price_to_earnings = statistics.mean(price_to_earning[:-3])
-#
-list_from_json = data['Collapsed']['rows'][3]['datum']
-#print (list_from_json)
-price_to_book = [float(i) for i in list_from_json]
-#print (sum(price_to_book[:-3]))
-print ("Average PB:",statistics.mean(price_to_book[:-3]))
-avg_price_to_book = statistics.mean(price_to_book[:-3])
+with open('stocks.txt', 'r') as stock_file:
+    stocks = {}
+    for l in stock_file:  
+        scrip,code = l.strip().split(':')
+        stocks[scrip] = code
+        #if scrip in stocks:
+            #stocks[scrip].append(code)
+        #else:
+            #stocks[scrip]=[code]
 
-#####################################################
-#   Get Current Values
-#####################################################
-current_price_to_sales = price_to_sales[-4]
-print ("Current PS:",current_price_to_sales)
-current_price_to_earnings = price_to_earning[-4]
-print ("Current PE:",current_price_to_earnings)
-current_price_to_book = price_to_book[-4]
-print ("Current PB:",current_price_to_book)
+#print (stocks)
+print ("----------------------------------------------------------------\n")
+for scrip, code in stocks.items():
+    URL = "https://api-global.morningstar.com/sal-service/v1/stock/valuation/v2/" + code + "?access_token="+ access_token
+    #URL = "https://api-global.morningstar.com/sal-service/v1/stock/valuation/v2/0p0000hj9s?access_token="+ access_token
+    #URL = "https://api-global.morningstar.com/sal-service/v1/stock/valuation/v2/0p0000c20b?access_token="+ access_token
+     
+    head = {"Accept":"applicaiton/json", "Content-type": "application/json", }
+            
+     
+    # defining a params dict for the parameters to be sent to the API
+    cookies = dict(mid='17423110563646529469',Hint='usw2e10')
+     
+    # sending get request and saving the response as response object
+    r = requests.get(url = URL, headers=head,cookies=cookies,verify=False)
+     
+    # extracting data in json format
+    data = r.json()
+    #print (data)
+     
+     
+    #####################################################
+    #   Get Historical Averages
+    #####################################################
 
-#####################################################
-#   Get Current Values
-#####################################################
-decision = 'SELL'
-possible_upside = 0.00
-if (current_price_to_book < avg_price_to_book):
-    decision = 'BUY'
-    possible_upside = (avg_price_to_book - current_price_to_book)/current_price_to_book *100
+    #print (data['Collapsed'])
+    list_from_json = data['Collapsed']['rows'][0]['datum']
+    #print (list_from_json)
+    price_to_sales = [float(i) for i in list_from_json if i is not None]
+    #print (sum(price_to_sales[:-3]))
+    print ("Average PS:",statistics.mean(price_to_sales[:-3]))
+    avg_price_to_sales = statistics.mean(price_to_sales[:-3])
+    #
+    #
+    list_from_json = data['Collapsed']['rows'][1]['datum']
+    #print (list_from_json)
+    price_to_earning = [float(i) for i in list_from_json if i is not None]
+    #print (sum(price_to_earning[:-3]))
+    print ("Average PE:",statistics.mean(price_to_earning[:-3]))
+    avg_price_to_earnings = statistics.mean(price_to_earning[:-3])
+    #
+    list_from_json = data['Collapsed']['rows'][3]['datum']
+    #print (list_from_json)
+    price_to_book = [float(i) for i in list_from_json if i is not None]
+    #print (sum(price_to_book[:-3]))
+    print ("Average PB:",statistics.mean(price_to_book[:-3]))
+    avg_price_to_book = statistics.mean(price_to_book[:-3])
 
-print ("Verdict on is",decision)
-print ("Possible Upside is: ",possible_upside)
+    #####################################################
+    #   Get Current Values
+    #####################################################
+    current_price_to_sales = price_to_sales[-4]
+    print ("Current PS:",current_price_to_sales)
+    current_price_to_earnings = price_to_earning[-4]
+    print ("Current PE:",current_price_to_earnings)
+    current_price_to_book = price_to_book[-4]
+    print ("Current PB:",current_price_to_book)
+
+    #####################################################
+    #   Get Current Values
+    #####################################################
+    decision = 'SELL'
+    possible_upside = 0.00
+    if (current_price_to_book < avg_price_to_book):
+        decision = 'BUY'
+        possible_upside = (avg_price_to_book - current_price_to_book)/current_price_to_book *100
+        print ("Possible Upside is: ",possible_upside)
+
+    print ("\n")
+    print ("Verdict on " + scrip + " is",decision)
+    print ("----------------------------------------------------------------\n")
